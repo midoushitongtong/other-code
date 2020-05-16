565840f493a691b586aef97b59d3ce5d4aa41bca
midoushitongtong <1092879991@qq.com>
2020-05-17 02:56:51 +0800

    chore: save

---
+++ learn-vue-component/src/assets/theme/gulpfile.js
@@ -0,0 +1,18 @@
+const { src, dest, watch } = require('gulp');
+const gulpSass = require('gulp-sass');
+
+/**
+ * 监听 scss 文件变化重写编译成 css
+ */
+function watchBuildScssTask() {
+  watch(['src/**/*.scss'], function () {
+    return buildScssTask();
+  });
+}
+
+function buildScssTask(cb) {
+  return src('src/**/*.scss').pipe(gulpSass()).pipe(dest('../../../dist/css/'));
+}
+
+module.exports.watchBuildScssTask = watchBuildScssTask;
+module.exports.buildScssTask = buildScssTask;

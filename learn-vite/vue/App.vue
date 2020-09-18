b61b3979f5d4f223c3cc7b8ab547c4f362f09f2f
YYC <1092879991@qq.com>
2020-09-18 18:23:02 +0800

    chore: save

---
+++ learn-vite/vue/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div @click="increment(1)">{{ count }}</div>
+</template>
+
+<script lang="ts">
+import { reactive, toRefs } from 'vue';
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+
+    const increment = (number) => {
+      state.count += number;
+    };
+
+    return {
+      ...toRefs(state),
+      increment,
+    };
+  },
+};
+</script>
+
+<style></style>

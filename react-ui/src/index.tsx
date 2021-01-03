89c6e5af72472b3db130eb6bf73a9effa9a73a44
YYC <1092879991@qq.com>
2021-01-03 23:02:47 +0800

    ...

--- react-ui/src/index.tsx
+++ react-ui/src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Test from './components/Test';
-import reportWebVitals from './reportWebVitals';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const a = 1;
-a = '2';
 ReactDOM.render(
   <React.StrictMode>
-    <Test />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -16,4 +14,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
